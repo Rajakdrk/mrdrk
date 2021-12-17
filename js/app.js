f30508fa532ecff40d487e2a6dc6e29f3b9867a1
Rajakdrk <rajakdrk99638@gmail.com>
2021-12-18 01:21:53 +0530

    major changes occurred
    Progress Circles namely Python, C, Java , and Web Development

--- js/app.js
+++ js/app.js
@@ -23,3 +23,49 @@ times.addEventListener('click', function(){
 
 
 });
+
+
+// Progression sectoin
+let number = document.getElementById("number");
+let counter = 0;
+setInterval(() => {
+    if(counter == 80){
+        clearInterval();
+    }else{
+        counter += 1;
+        number.innerHTML = counter + "%";
+    }
+}, 24);
+
+let number2 = document.getElementById("number2");
+let counter2 = 0;
+setInterval(() => {
+    if(counter2 == 70){
+        clearInterval();
+    }else{
+        counter2 += 1;
+        number2.innerHTML = counter2 + "%";
+    }
+}, 24);
+
+let number3 = document.getElementById("number3");
+let counter3 = 0;
+setInterval(() => {
+    if(counter3 == 50){
+        clearInterval();
+    }else{
+        counter3 += 1;
+        number3.innerHTML = counter3 + "%";
+    }
+}, 24);
+
+let number4 = document.getElementById("number4");
+let counter4 = 0;
+setInterval(() => {
+    if(counter4 == 50){
+        clearInterval();
+    }else{
+        counter4 += 1;
+        number4.innerHTML = counter4 + "%";
+    }
+}, 24);
